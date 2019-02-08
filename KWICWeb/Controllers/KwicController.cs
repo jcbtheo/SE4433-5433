ffe21d34b7493f6a87094efc1bc6d0fe953072b4
Jacob Theobald <jcbtheo@gmail.com>
2019-02-08 09:26:04 -0600

    Initial project upload. Basic web page and ajax call.

---
+++ KWICWeb/Controllers/KwicController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KWICWeb.Controllers
+{
+    [Route("api/[controller]")]
+    public class KwicController : Controller
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody]string inputData)
+        {
+            try
+            {
+                inputData += "!!!";
+                return Ok(new { data = inputData });
+            }
+            catch
+            {
+                return BadRequest("There was a problem analyzing your input. Please try again.");
+            }
+        }
+    }
+}
